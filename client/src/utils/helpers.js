2e9e28c44d459aa80fc3f92577913a384f69f85e
maribels_are_ringing <maribel.duron86@gmail.com>
2021-07-05 20:45:10 -0700

    installed missing deps and updated to redux

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,12 +4,6 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
-export function pluralize(name, count) {
-  if (count === 1) {
-    return name;
-  }
-  return name + 's';
-}
 
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
